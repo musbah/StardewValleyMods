4a2090b5e4c268fe43625253aa722139b4d71614
Musbah Sinno <musbahsinno@hotmail.com>
2017-07-14 19:50:18 +0300

    Reorganized the whole solution to accomodate multiple mods and added a new mod folder.

---
+++ CookingOrganizer/ModEntry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace CookingOrganizer
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            ControlEvents.KeyPressed += this.ControlEvents_KeyPress;
+        }
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>The method invoked when the player presses a keyboard button.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void ControlEvents_KeyPress(object sender, EventArgsKeyPressed e)
+        {
+            if (Context.IsWorldReady) // save is loaded
+            {
+                this.Monitor.Log($"{Game1.player.name} pressed {e.KeyPressed}.");
+            }
+        }
+    }
+}
